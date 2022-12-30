b8fc4d6e9be2e9d1e174c370f9a2cdb32256a170
近村修斗 <scmarudekaze@gmail.com>
2022-12-31 01:58:46 +0900

    atoms追加

--- src/themes/colors.ts
+++ src/themes/colors.ts
@@ -4,11 +4,14 @@ const colors = {
   primaryLight: '#6573c3',
   secondary: '#f50057',
   secondaryDark: '#ab003c',
+  secondaryLight: '#f73378',
   border: '#cdced2',
-  danger: '#edlc24',
+  danger: '#ed1c24',
+  dangerDark: '#a50d12',
   gray: '#6b6b6b',
   black: '#000000',
   white: '#ffffff',
-}
+  text: '#000000',
+} as const
 
 export default colors
